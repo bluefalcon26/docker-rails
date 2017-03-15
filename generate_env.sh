e4eea342596ab0d80e1bedece80865fb85d59da8
root <root@bluebird.aasteel.net>
2017-03-15 14:50:13 -0500

    Add container_name parameter to generator

--- generate_env.sh
+++ generate_env.sh
@@ -15,6 +15,7 @@ function usage
 arg_http_proxy=
 arg_local_user_id=
 arg_local_group_id=
+arg_rails_name=
 arg_rails_external_port=
 arg_rails_run_env=
 arg_rails_secret_key_base=
@@ -24,20 +25,23 @@ while [ -n "$1" ]; do
                             arg_http_proxy=$1
                             ;;
     -u | --user )           shift
-                            arg_local_user_id=$1                                                                                                                                                                              
-                            ;;                                                                                                                                                                                                
-    -g | --group )          shift                                                                                                                                                                                             
-                            arg_local_group_id=$1                                                                                                                                                                             
-                            ;;                                                                                                                                                                                                
-    -p | --port )           shift                                                                                                                                                                                             
-                            arg_rails_external_port=$1                                                                                                                                                                        
-                            ;;                                                                                                                                                                                                
-    -e | --environment )    shift                                                                                                                                                                                             
-                            arg_rails_run_env=$1                                                                                                                                                                              
-                            ;;                                                                                                                                                                                                
-    -s | --secret )         shift                                                                                                                                                                                             
-                            arg_rails_secret_key_base=$1                                                                                                                                                                      
-                            ;;                                                                                                                                                                                                
+                            arg_local_user_id=$1
+                            ;;
+    -g | --group )          shift
+                            arg_local_group_id=$1
+                            ;;
+    -n | --name )           shift
+                            arg_rails_name=$1
+                            ;;
+    -p | --port )           shift
+                            arg_rails_external_port=$1
+                            ;;
+    -e | --environment )    shift
+                            arg_rails_run_env=$1
+                            ;;
+    -s | --secret )         shift
+                            arg_rails_secret_key_base=$1
+                            ;;
     -h | --help )           usage
                             exit
                             ;;
@@ -61,7 +65,6 @@ fi
 
 # 1. HTTP_PROXY
 # Default: blank
-
 if [ -n "$arg_http_proxy" ]; then
   echo "HTTP_PROXY=$arg_http_proxy" > .env
 elif [ ! $HTTP_PROXY = "" ]; then
@@ -73,56 +76,61 @@ else
 fi
 
 # 2. LOCAL_USER_ID
-# Default: current user id
-
+# Default: *current user id
 if [ -n "$arg_local_user_id" ]; then
   echo "LOCAL_USER_ID=$arg_local_user_id" >> .env
 elif [ ! $LOCAL_USER_ID = "" ]; then
-  echo "LOCAL_USER_ID=${LOCAL_USER_ID}" >> .env
+  echo "LOCAL_USER_ID=$LOCAL_USER_ID" >> .env
 else
   echo "LOCAL_USER_ID=$(id -u)" >> .env
 fi
 
 # 3. LOCAL_GROUP_ID
-# Default: current group id
-
+# Default: *current group id
 if [ -n "$arg_local_group_id" ]; then
   echo "LOCAL_GROUP_ID=$arg_local_group_id" >> .env
 elif [ ! $LOCAL_GROUP_ID = "" ]; then
-  echo "LOCAL_GROUP_ID=${LOCAL_GROUP_ID}" >> .env
+  echo "LOCAL_GROUP_ID=$LOCAL_GROUP_ID" >> .env
 else
   echo "LOCAL_GROUP_ID=$(id -g)" >> .env
 fi
 
-# 4. RAILS_EXTERNAL_PORT
-# Default: 3000
+# 4. RAILS_NAME
+# Default: *current directory name
+if [ -n "$arg_rails_name" ]; then
+  echo "RAILS_NAME=$arg_rails_name" >> .env
+elif [ ! $RAILS_NAME = "" ]; then
+  echo "RAILS_NAME=$RAILS_NAME" >> .env
+else
+  echo "RAILS_NAME=rails_${PWD##*/}" >> .env
+fi
 
+# 5. RAILS_EXTERNAL_PORT
+# Default: 3000
 if [ -n "$arg_rails_external_port" ]; then
   echo "RAILS_EXTERNAL_PORT=$arg_rails_external_port" >> .env
 elif [ ! $RAILS_EXTERNAL_PORT = "" ]; then
-  echo "RAILS_EXTERNAL_PORT=${RAILS_EXTERNAL_PORT}" >> .env
+  echo "RAILS_EXTERNAL_PORT=$RAILS_EXTERNAL_PORT" >> .env
 else
   echo "RAILS_EXTERNAL_PORT=3000" >> .env
 fi
 
-# 5. RAILS_RUN_ENV
+# 6. RAILS_RUN_ENV
 # Default: development
-
 if [ -n "$arg_rails_run_env" ]; then
   echo "RAILS_RUN_ENV=$arg_rails_run_env" >> .env
 elif [ ! $RAILS_RUN_ENV = "" ]; then
-  echo "RAILS_RUN_ENV=${RAILS_RUN_ENV}" >> .env
+  echo "RAILS_RUN_ENV=$RAILS_RUN_ENV" >> .env
 else
   echo "RAILS_RUN_ENV=development" >> .env
 fi
 
-# 6. RAILS_SECRET_KEY_BASE
+# 7. RAILS_SECRET_KEY_BASE
 # Default: *generate new secret with \rake secret\
-
 if [ -n "$arg_rails_secret_key_base" ]; then
   echo "RAILS_SECRET_KEY_BASE=$arg_rails_secret_key_base" >> .env
 elif [ ! $RAILS_SECRET_KEY_BASE = "" ]; then
-  echo "RAILS_SECRET_KEY_BASE=${RAILS_SECRET_KEY_BASE}" >> .env
+  echo "RAILS_SECRET_KEY_BASE=$RAILS_SECRET_KEY_BASE" >> .env
 else
   echo "RAILS_SECRET_KEY_BASE=$(rake secret)" >> .env
 fi
